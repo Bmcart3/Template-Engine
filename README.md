e5004a5b242eef9fe298b1dd57d26b5f1cf08998
bmcart3 <bmcart3@gmail.com>
2020-05-30 22:14:32 -0400

    Finished inquirer prompts and now writes to team.html inside output folder.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # templateEngine
 Software engineering team generator command line application.
+In this application, the user will be able to create an entire software engineering team through command line prompts. Using the Inquirer NPM, the user can answer questions based on the selected roles and then an html file will be created which will show the entire software engineering team in a visually appealing interface.
+
+This application requires extensive use of Inquirer and also Jest which allows us to test various aspects of classes created for this application. I believe the application will be a very useful template in the future..
--- app.js
+++ app.js
@@ -2,41 +2,12 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const team = [];
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
-//do the inquirer prompts here.
+//Initial inquirer prompts
 const initialQuestions = () => {
     inquirer.prompt([
         {
@@ -81,7 +52,7 @@ async function roleChecker(data) {
         }
     ]);
     //creates new engineer, intern, or manager
-    //console.log(response); //response.answer will get the value (answer) of the unique question.
+    //response.answer will get the value (answer) of the unique question.
     let newGuy;
     if (data.role === "Engineer") {
         newGuy = new Engineer(data.name, data.id, data.email, response.answer)
@@ -90,13 +61,31 @@ async function roleChecker(data) {
     } else {
         newGuy = new Manager(data.name, data.id, data.email, response.answer)
     };
-    //console.log(newGuy);
     team.push(newGuy);
-    //console.log(team);
+    addMember();
 };
 
-//Need to allow them to pick another employeeif they want... then recycle initial questions.
-//When team is complete need to use the render function and writefile...
+//Determines if the user wants another member on the team or not. If yes, reruns initial questions. If not, renders the team and writes a new file in output folder.
+async function addMember() {
+    const response = await inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Would you like to add another team member?",
+            name: "newMember"
+        }
+    ]);
+    if (response.newMember) {
+        initialQuestions();
+    } else {
+        render(team);
+        fs.writeFile("./output/team.html", render(team), function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success!");
+        });
+    };
+};
 
 initialQuestions();
 
